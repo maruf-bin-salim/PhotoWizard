79a79d4483257693481c0fbdaf2c4ad6b2a4fd02
maruf-bin-salim <marufbinsalim01@gmail.com>
2023-10-21 04:33:51 +0600

    U

---
+++ JS/caman.processing.js
@@ -0,0 +1,61 @@
+Caman.Filter.register("blackAndWhite", function (grey) {
+  this.greyscale();
+  this.sepia(10);
+  this.exposure(10);
+  this.contrast(15);
+  return this.vignette("60%", 35);
+});
+
+Caman.Filter.register("bright", function (grey) {
+  this.brightness(7);
+  this.exposure(5);
+  this.contrast(16);
+  return this.vibrance(7);
+});
+
+(function () {
+  var busy, caman, filters, render;
+
+  caman = null;
+  filters = {};
+  busy = false;
+
+  render = function () {
+    if (busy) {
+      return;
+    }
+    busy = true;
+    caman.revert(false);
+    for (var filter in filters) {
+      if (filters.hasOwnProperty(filter)) {
+        var value = parseFloat(filters[filter], 10);
+        if (value === 0) {
+          continue;
+        }
+        caman[filter](value);
+      }
+    }
+    caman.render(function () {
+      busy = false;
+    });
+  };
+
+  document.addEventListener("DOMContentLoaded", function () {
+    if (!document.getElementById("processedImageElement")) {
+      return;
+    }
+    caman = Caman('#processedImageElement');
+    var filterInputs = document.querySelectorAll('.FilterSetting input');
+    filterInputs.forEach(function (input) {
+      var filter = input.getAttribute('data-filter');
+      filters[filter] = input.value;
+      input.addEventListener('input', function () {
+        var filter = this.getAttribute('data-filter');
+        var value = this.value;
+        filters[filter] = value;
+        this.nextElementSibling.innerHTML = value;
+        render();
+      });
+    });
+  });
+})();
---
+++ JS/processing-core.js
@@ -0,0 +1,104 @@
+document.addEventListener("DOMContentLoaded", handleDocumentLoad);
+let fileInput = null;
+let imageElement = null;
+let processedImageElement = null;
+let clearButton = null;
+let saveButton = null;
+let imageContainer = null;
+
+function handleDocumentLoad() {
+
+    // Log that the document has loaded.
+    console.log("Document loaded.");
+
+    fileInput = document.getElementById("fileInput");
+    imageElement = document.getElementById("imageElement");
+    processedImageElement = document.getElementById("processedImageElement");
+    clearButton = document.getElementById("clearButton");
+    saveButton = document.getElementById("saveButton");
+    imageContainer = document.getElementById("imageContainer");
+
+    // Check if the file input and image element exist.
+    if (!fileInput || !imageElement || !processedImageElement) {
+        return;
+    }
+
+    // Log that the file input and image element exist.
+    console.log("File input and image element exist.");
+
+    // Add an event listener to the file input.
+    fileInput.addEventListener("change", handleImageSelection);
+    // Add an event listener to the clear button.
+    clearButton.addEventListener("click", handleClearButtonClick);
+    // Add an event listener to the save button.
+    saveButton.addEventListener("click", handleSaveButtonClick);
+
+    updateImageSource(""); // Clear the image source.
+}
+
+// Handle the save button click event.
+function handleSaveButtonClick(e) {
+
+
+    // Log that the save button was clicked.
+    console.log("Save button clicked.");
+
+    // Check if the processed image element exists.
+    if(imageContainer.style.display == "none") {
+        alert("Please select an image first.");
+        return;
+    }
+    // Save the image.
+    // - Create a canvas element.
+    // - Set the canvas width and height to the image width and height.
+    // - Draw the image onto the canvas.
+    // - Convert the canvas to a data URL.
+    // - Create a link element.
+    // - Set the link element download attribute to the file name.
+    // - Set the link element href attribute to the data URL.
+    // - Click the link element.
+    processedImageElement = document.getElementById("processedImageElement");
+    const dataURL = processedImageElement.toDataURL("image/png");
+    const link = document.createElement("a");
+    link.download = "image.png";
+    link.href = dataURL;
+    link.click();
+}
+
+// Handle the clear button click event.
+function handleClearButtonClick(e) {
+
+    // Clear the file input.
+    fileInput.value = "";
+
+    // Clear the image source.
+    updateImageSource("");
+}
+
+// Handle the image selection event.
+function handleImageSelection(e) {
+
+    const selectedFile = e.target.files[0];
+
+    // Check if a file was selected and if the file is an image
+    if (!selectedFile || !selectedFile.type.startsWith("image/")) {
+        updateImageSource("");
+        alert("Please select a valid image file.");
+        return;
+    }
+
+    // Create a URL for the selected image and update the image source.
+    const imageURL = URL.createObjectURL(selectedFile);
+    console.log(imageURL);
+    updateImageSource(imageURL);
+}
+
+function updateImageSource(source) {
+
+    if (!imageElement || !processedImageElement) { return; }
+
+    imageContainer.style.display = (source == "") ? "none" : "flex";
+
+    imageElement.src = source;
+    processedImageElement.src = source;
+}
